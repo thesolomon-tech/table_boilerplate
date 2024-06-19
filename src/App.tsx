5bb638dab60908ffb6f48f119849141131fe0204
Solomon Khoury <solomon@thesolomon.tech>
2024-06-19 20:37:57 +1000

    Setup Complete

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ExampleWithProviders from './Components/Table'
+
+function App() {
+
+  return (<div>
+    <ExampleWithProviders />
+  </div>
+  )
+}
+
+export default App
